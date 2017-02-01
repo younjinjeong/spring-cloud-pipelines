3eda518cca6bc693099436dfdf59d69c17a5ee4b
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-02-01 15:49:49 +0100

    Added retrieval of artifactid

--- common/src/main/bash/prod_complete.sh
+++ common/src/main/bash/prod_complete.sh
@@ -4,6 +4,8 @@ set -e
 
 source pipeline.sh || echo "No pipeline.sh found"
 
+projectArtifactId=$( retrieveArtifactId )
+
 # Log in to CF to start deployment
 logInToCf "${REDOWNLOAD_INFRA}" "${CF_PROD_USERNAME}" "${CF_PROD_PASSWORD}" "${CF_PROD_ORG}" "${CF_PROD_SPACE}" "${CF_PROD_API_URL}"
 
